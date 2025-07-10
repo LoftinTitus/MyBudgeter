442599df13d26eea79a87276db726da14ea4eb9b
Ty Loftin <tyloftin4@gmail.com>
2025-07-10 11:18:41 -0500

    fixed issue 1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: process.env.NODE_ENV === 'production' ? '/MyBudgeter/' : '/',
-  root: '.',
   build: {
     outDir: 'dist'
   }
